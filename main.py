9a88d742433a3d50765753cea72d8563fbd0d71d
Lyozz <nomail.ru>
2023-01-17 23:40:44 +0300

    added a new function in main.py

--- main.py
+++ main.py
@@ -3,35 +3,43 @@ import requests
 import json
 import os
 
-url = "https://health-diet.ru/table_calorie/"
+# url = "https://health-diet.ru/table_calorie/"
 
-headers = {
-    "Accept": "*/*",
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 YaBrowser/22.11.5.715 Yowser/2.5 Safari/537.36"
-}
+# headers = {
+#     "Accept": "*/*",
+#     "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 YaBrowser/22.11.5.715 Yowser/2.5 Safari/537.36"
+# }
 
-r = requests.get(url, headers = headers)
+# r = requests.get(url, headers = headers)
 
-soup = BeautifulSoup(r.text, "lxml")
-types_food = soup.find_all("a", class_="mzr-tc-group-item-href")
+# soup = BeautifulSoup(r.text, "lxml")
+# types_food = soup.find_all("a", class_="mzr-tc-group-item-href")
 
-categories = {}
+# categories = {}
 
-for item in types_food:
-    text_item = item.text
-    link_item = "https://health-diet.ru" + item.get("href")
-    categories[text_item] = link_item
+# for item in types_food:
+#     text_item = item.text
+#     link_item = "https://health-diet.ru" + item.get("href")
+#     categories[text_item] = link_item
 
-for category, link in categories.items():  
-    rep = [", "," ", ",", "-", "'"]
-    for item3 in rep:
-        if (item3 in category):
-            category = category.replace(item3, "_")
-    with open(f"{category}.json", "w", encoding="utf-8") as file:
-        json.dump(categories, file, ensure_ascii=False, indent=4)
+# for category, link in categories.items():  
+#     rep = [", "," ", ",", "-", "'"]
+#     for item3 in rep:
+#         if (item3 in category):
+#             category = category.replace(item3, "_")
+#     with open(f"{category}.json", "w", encoding="utf-8") as file:
+#         json.dump(categories, file, ensure_ascii=False, indent=4)
 
+# ==========================================================================
 
+x = int(input("Введите размерность массива по столбцам: ")) # 5
+y = int(input("Введите размерность массива по строкам: ")) # 3
 
-
-
-
+count = 1
+arr = [[0] * x for i in range(y)]
+for i in range(0, len(arr)):
+    for j in range(0, len(arr[i])):
+        arr[i][j] = count
+        print(arr[i][j], end="  ") if (count < 10) else print(arr[i][j], end=" ")
+        count += 1
+    print("", end="\n")
